5608d265f985a05ef4623bc90295f981947cdebe
Lisa Stillwell <lisastillwell@bellsouth.net>
2022-03-14 17:08:18 -0400

    some bug fixes in server

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,31 +1,24 @@
 const { AuthenticationError } = require('apollo-server-express');
-const { User, Book } = require('../models');
+const { User } = require('../models');
 const { signToken } = require('../utils/auth');
 
 const resolvers = {
-    // Query: {
-    //     user: async (parent, { username }) => {
-    //         const params = username ? { username } : {};
-    //         return User.find(params).sort({ createdAt: -1 });
-    //     },
-    // }
+
     Query: {
         getSingleUser: async (parent, { user, params }) => {
             return User.findOne({
                 $or: [{ _id: user ? user._id : params.id }, { username: params.username }],
             });
-        },
+        }
     },
 
     Mutation: {
-
         addUser: async (parent, args) => {
             const user = await User.create(args);
             const token = signToken(user);
       
             return { token, user };
         },
-
         login: async (parent, { email, password }) => {
             const user = await User.findOne({ email });
       
@@ -42,31 +35,34 @@ const resolvers = {
             const token = signToken(user);
             return { token, user };
         },
-
-        saveBook: async (parent, { bookId }, context) => {
+        saveBook: async (parent, { bookData }, context) => {
             if (context.user) {
-              const updatedUser = await User.findOneAndUpdate(
-                { _id: context.user._id },
-                { $addToSet: { savedBooks: bookId } },
-                { new: true }
-              ).populate('savedBooks');
+                const updatedUser = await User.findByIdAndUpdate(
+                    { _id: context.user._id },
+                    { $push: { savedBooks: bookData } },
+                    { new: true }
+                ).populate('savedBooks');
       
               return updatedUser;
             }
       
             throw new AuthenticationError('You need to be logged in!');
         },
-
         deleteBook: async (parent, { bookId }, context) => {
-            const updatedUser = await User.findOneAndUpdate(
-              { _id: context.user._id },
-              { $pull: { savedBooks: { bookId: params.bookId } } },
-              { new: true }
-            );
-            if (!updatedUser) {
-              return res.status(404).json({ message: "Couldn't find user with this id!" });
+            if(context.user) {
+                const updatedUser = await User.findOneAndUpdate(
+                { _id: context.user._id },
+                { $pull: { savedBooks: { bookId } } },
+                { new: true }
+                );
+                if (!updatedUser) {
+                return res.status(404).json({ message: "Couldn't find user with this id!" });
+                }
+
+                return (updatedUser);
             }
-            return res.json(updatedUser);
-          },
+            
+            throw new AuthenticationError('You need to be logged in!');
+        }
     }
 }
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,25 +3,38 @@ const { gql } = require('apollo-server-express');
 
 // create our typeDefs
 const typeDefs = gql`
-type Book {
-    _id: ID
-    bookId: String
-    authors: [String]
-    description: String
-    image: String
-    link: String
-    title: String
+type Query {
+    me: User
+}
+type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(bookData: BookInput!): User
+    deleteBook(bookId: ID!): User
 }
 type User {
+    _id: ID!
     username: String
     email: String
-    password: String
+    bookCount: Int
     savedBooks: [Book]
 }
-type Query {
-    user: User
+type Book {
+    bookId: ID!
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
 }
-
+input BookInput {
+    authors: [String]
+    description: String
+    bookId: String!
+    title: String!
+    image: String
+    link: String
+}
 type Auth {
     token: ID!
     user: User
